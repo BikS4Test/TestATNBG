84f122b9cf1b0bea208497fbfdb26c2e87eee0e6
AutoCode <autocode@papergo.io>
2024-07-04 07:36:02 +0300

    update from autocode on 2024-07-04 07:36:02.852400

---
+++ Types/APIEndpointTag.cs
@@ -0,0 +1,10 @@
+
+namespace ProjectName.Types
+{
+    public class APIEndpointTag
+    {
+        public Guid Id { get; set; }
+        public Guid APIEndpointId { get; set; }
+        public Guid APITagId { get; set; }
+    }
+}
