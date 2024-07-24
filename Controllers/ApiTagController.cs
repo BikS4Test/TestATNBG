23c7a0fd40df7bcdd198e88034a38f0fcc4cd40c
AutoCode <autocode@papergo.io>
2024-07-24 13:34:35 +0300

    update from autocode on 2024-07-24 13:34:35.371244

--- Controllers/ApiTagController.cs
+++ Controllers/ApiTagController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using ProjectName.Types;
 using ProjectName.Interfaces;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ProjectName.Controllers
@@ -11,60 +12,47 @@ namespace ProjectName.Controllers
     public class ApiTagController : ControllerBase
     {
         private readonly IApiTagService _apiTagService;
+        private readonly ISafeExecutor _safeExecutor;
 
-        public ApiTagController(IApiTagService apiTagService)
+        public ApiTagController(IApiTagService apiTagService, ISafeExecutor safeExecutor)
         {
             _apiTagService = apiTagService;
+            _safeExecutor = safeExecutor;
         }
 
         [HttpPost("create")]
         public async Task<IActionResult> CreateApiTag([FromBody] Request<CreateApiTagDto> request)
         {
-            return await SafeExecutor.ExecuteAsync(async () =>
-            {
-                var result = await _apiTagService.CreateApiTag(request.Payload);
-                return Ok(new Response<string> { Payload = result });
-            });
+            return await _safeExecutor.ExecuteAsync(async () => 
+                Ok(await _apiTagService.CreateApiTag(request.Payload)));
         }
 
         [HttpPost("get")]
         public async Task<IActionResult> GetApiTag([FromBody] Request<ApiTagRequestDto> request)
         {
-            return await SafeExecutor.ExecuteAsync(async () =>
-            {
-                var result = await _apiTagService.GetApiTag(request.Payload);
-                return Ok(new Response<ApiTag> { Payload = result });
-            });
+            return await _safeExecutor.ExecuteAsync(async () => 
+                Ok(await _apiTagService.GetApiTag(request.Payload)));
         }
 
         [HttpPost("update")]
         public async Task<IActionResult> UpdateApiTag([FromBody] Request<UpdateApiTagDto> request)
         {
-            return await SafeExecutor.ExecuteAsync(async () =>
-            {
-                var result = await _apiTagService.UpdateApiTag(request.Payload);
-                return Ok(new Response<string> { Payload = result });
-            });
+            return await _safeExecutor.ExecuteAsync(async () => 
+                Ok(await _apiTagService.UpdateApiTag(request.Payload)));
         }
 
         [HttpPost("delete")]
         public async Task<IActionResult> DeleteApiTag([FromBody] Request<DeleteApiTagDto> request)
         {
-            return await SafeExecutor.ExecuteAsync(async () =>
-            {
-                var result = await _apiTagService.DeleteApiTag(request.Payload);
-                return Ok(new Response<bool> { Payload = result });
-            });
+            return await _safeExecutor.ExecuteAsync(async () => 
+                Ok(await _apiTagService.DeleteApiTag(request.Payload)));
         }
 
         [HttpPost("list")]
         public async Task<IActionResult> GetListApiTag([FromBody] Request<ListApiTagRequestDto> request)
         {
-            return await SafeExecutor.ExecuteAsync(async () =>
-            {
-                var result = await _apiTagService.GetListApiTag(request.Payload);
-                return Ok(new Response<List<ApiTag>> { Payload = result });
-            });
+            return await _safeExecutor.ExecuteAsync(async () => 
+                Ok(await _apiTagService.GetListApiTag(request.Payload)));
         }
     }
 }
