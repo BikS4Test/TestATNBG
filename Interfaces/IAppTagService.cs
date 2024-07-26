2bc9cea346f68c3402a8ba7ee6abcfe278c27d31
AutoCode <autocode@papergo.io>
2024-07-26 22:51:00 +0300

    update from autocode on 2024-07-26 22:51:00.284012

--- Interfaces/IAppTagService.cs
+++ Interfaces/IAppTagService.cs
@@ -13,36 +13,36 @@ namespace ProjectName.Interfaces
         /// <summary>
         /// Creates a new application tag.
         /// </summary>
-        /// <param name="createAppTagDto">The data transfer object containing the information for the new application tag.</param>
+        /// <param name="createAppTagDto">Data transfer object for creating an application tag.</param>
         /// <returns>A string representing the result of the creation operation.</returns>
         Task<string> CreateAppTag(CreateAppTagDto createAppTagDto);
 
         /// <summary>
         /// Retrieves an application tag based on the provided request data.
         /// </summary>
-        /// <param name="requestDto">The data transfer object containing the request information for retrieving the application tag.</param>
-        /// <returns>An AppTag object representing the retrieved application tag.</returns>
+        /// <param name="requestDto">Data transfer object containing request details for retrieving an application tag.</param>
+        /// <returns>An instance of AppTag representing the retrieved tag.</returns>
         Task<AppTag> GetAppTag(AppTagRequestDto requestDto);
 
         /// <summary>
         /// Updates an existing application tag.
         /// </summary>
-        /// <param name="updateAppTagDto">The data transfer object containing the updated information for the application tag.</param>
+        /// <param name="updateAppTagDto">Data transfer object for updating an application tag.</param>
         /// <returns>A string representing the result of the update operation.</returns>
         Task<string> UpdateAppTag(UpdateAppTagDto updateAppTagDto);
 
         /// <summary>
-        /// Deletes an application tag based on the provided request data.
+        /// Deletes an application tag.
         /// </summary>
-        /// <param name="deleteAppTagDto">The data transfer object containing the information for deleting the application tag.</param>
-        /// <returns>A boolean indicating whether the deletion was successful.</returns>
+        /// <param name="deleteAppTagDto">Data transfer object for deleting an application tag.</param>
+        /// <returns>A boolean indicating the success of the deletion operation.</returns>
         Task<bool> DeleteAppTag(DeleteAppTagDto deleteAppTagDto);
 
         /// <summary>
         /// Retrieves a list of application tags based on the provided request data.
         /// </summary>
-        /// <param name="listAppTagRequestDto">The data transfer object containing the request information for retrieving the list of application tags.</param>
-        /// <returns>A list of AppTag objects representing the retrieved application tags.</returns>
-        Task<List<AppTag>> GetListAppTag(ListAppTagRequestDto listAppTagRequestDto);
+        /// <param name="requestDto">Data transfer object containing request details for retrieving a list of application tags.</param>
+        /// <returns>A list of AppTag instances representing the retrieved tags.</returns>
+        Task<List<AppTag>> GetListAppTag(ListAppTagRequestDto requestDto);
     }
 }
