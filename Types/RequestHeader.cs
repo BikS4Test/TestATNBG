a3ff6bf72b4e4fc8736a57748498149cfb087d68
AutoCode <autocode@papergo.io>
2024-07-27 01:53:05 +0300

    update from autocode on 2024-07-27 01:53:05.642793

--- Types/RequestHeader.cs
+++ Types/RequestHeader.cs
@@ -4,7 +4,7 @@ namespace ProjectName.Types
     public class RequestHeader
     {
         public Guid Id { get; set; }
-        public Guid Application { get; set; } = new Guid("03FC0B90-DFAD-11EE-8D86-0800200C9A66");
+        public Guid Application { get; set; }
         public string Bank { get; set; }
         public Guid UserId { get; set; }
     }
