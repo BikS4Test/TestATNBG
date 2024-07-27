3bcc9133803731151d75998002864ed65d51adee
AutoCode <autocode@papergo.io>
2024-07-27 11:31:55 +0300

    update from autocode on 2024-07-27 11:31:55.855736

--- Types/UpdateAppTagDto.cs
+++ Types/UpdateAppTagDto.cs
@@ -4,6 +4,6 @@ namespace ProjectName.Types
     public class UpdateAppTagDto
     {
         public Guid? Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
     }
 }
